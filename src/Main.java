b166d940f28a91897159540ef79502ce3a43ab53
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-11-17 19:34:02 -0500

    Added a user created version to reverse an array.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(reverseArray(9)));
-        System.out.println(Arrays.toString(reverseArray(6)));
-        System.out.println(Arrays.toString(reverseArray(5)));
-        System.out.println(Arrays.toString(reverseArray(3)));
+        System.out.println(Arrays.toString(reverseRandomArray(9)));
+        System.out.println(Arrays.toString(reverseRandomArray(6)));
+        System.out.println(Arrays.toString(reverseRandomArray(5)));
+        System.out.println(Arrays.toString(reverseRandomArray(3)));
+
+        System.out.println(Arrays.toString(getIntegers(5)));
 
     }
 
-    public static int[] reverseArray(int arrayLength) {
+    public static int[] reverseRandomArray(int arrayLength) {
 
         int[] array = randomArray(arrayLength);
         Arrays.sort(array);
@@ -36,6 +39,40 @@ public class Main {
             randomArray[i] = random.nextInt(100);
         }
         return randomArray;
+    }
+
+    public static int[] reverseArray(int[] array) {
+
+        Arrays.sort(array);
+        int lengthOfArray = array.length;
+        int[] descendingArray = new int[lengthOfArray];
+
+        for(int i = 0; i < array.length; i ++) {
+            descendingArray[i] = array[lengthOfArray - 1];
+            lengthOfArray -= 1;
+        }
+        return descendingArray;
+    }
+
+    public static int[] getIntegers(int arrayLength) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] intArray = new int[arrayLength];
+        int validInt;
+
+        System.out.printf("You have selected to create your own integer with a length of %d%n!"
+                , arrayLength);
+        for(int i = 0; i < arrayLength; i++) {
+            System.out.println("Enter an integer for an Array!");
+            String nextInt = scanner.nextLine();
+            validInt = Integer.parseInt(nextInt);
+            intArray[i] = validInt;
+        }
+        System.out.println(Arrays.toString(intArray));
+        return reverseArray(intArray);
+
+
+
     }
 
 }
